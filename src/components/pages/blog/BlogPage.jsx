9b2b399d98f527e682b7a78153a1298391d1695c
hulkdx <hulktroll@gmail.com>
2022-11-15 22:06:59 +0200

    Update css related to height issue on the phone

--- src/components/pages/blog/BlogPage.jsx
+++ src/components/pages/blog/BlogPage.jsx
@@ -5,7 +5,13 @@ import data from './data';
 import './BlogPage.css';
 
 function BlogPage() {
-  return <div className="blogs">{data.map((d) => Blog(d))}</div>;
+  return (
+    <div className="blogs">
+      <div className="empty-block" />
+      {data.map((d) => Blog(d))}
+      <div className="empty-block" />
+    </div>
+  );
 }
 
 export default BlogPage;
