626ac564ad87af0a5066fa56ff48a6a177484500
hulkdx <hulktroll@gmail.com>
2023-04-08 12:57:09 +0300

    Small change

--- src/components/pages/findprofessional/FindProfessionalPage.jsx
+++ src/components/pages/findprofessional/FindProfessionalPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable max-len */
 import React from 'react';
 
 import './FindProfessionalPage.css';
@@ -15,7 +17,8 @@ function FindProfessionalPage() {
           <p>One of the unique features of Find Professional is the ability to schedule one-on-one sessions with your coach. These sessions are paid, so you know that your coach is committed to helping you achieve your goals. Whether you need help with a specific project or want general guidance, our coaches are here to help.</p>
           <p>But Find Professional isn't just for individuals looking for coaching. Our app is also an excellent resource for professionals looking to make some extra money. By answering questions from our users, professionals can earn money for their expertise. It's a win-win situation - our users get the help they need, and professionals get paid for their knowledge.</p>
           {/* <p>So what are you waiting for? Download Find Professional today and connect with coaches who can help you achieve your goals. Our app is available on both iOS and Android, so no matter what device you use, you can start benefiting from personalized coaching right away.</p> */}
-          <p>At Find Professional, everyone deserves the chance to reach their full potential. With our app, you can get the coaching you need to take your skills to the next level. Whether you want to improve your programming skills, get in shape, or want guidance on your career path, our coaches are here to help.
+          <p>
+            At Find Professional, everyone deserves the chance to reach their full potential. With our app, you can get the coaching you need to take your skills to the next level. Whether you want to improve your programming skills, get in shape, or want guidance on your career path, our coaches are here to help.
             {/* Download Find Professional today and start your journey toward success! */}
           </p>
         </div>
