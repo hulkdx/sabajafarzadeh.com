4d7ee2d2b63c568d361bd5ad1665c32892fcf1ba
hulkdx <hulktroll@gmail.com>
2025-12-20 18:01:58 +0200

    Updated NavBar

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import RouteList from '../RouteList';
-import RouteListLeft from '../RouteListLeft';
-
+import { NavLink as Link } from 'react-router-dom';
 import './Navbar.css';
 
 function NavBar() {
   return (
     <header className="navbar-header">
       <nav className="navbar-container">
-        <div className="navbar-items-container-left">
-          <RouteListLeft />
-        </div>
-        <div className="navbar-items-container">
-          <RouteList />
+        <div className="navbar-inner">
+          {/* Left: Brand */}
+          <Link to="/findprofessional" className="navbar-brand">
+            <span className="navbar-logo-badge" aria-hidden="true" />
+            <span>Find Professional</span>
+          </Link>
+
+          {/* Right: Links */}
+          <ul className="navbar-links">
+            <li>
+              <Link to="/" exact activeClassName="active">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active">
+                About
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
